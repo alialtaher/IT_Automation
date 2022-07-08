d129f49f62e62b880f22951f13be1525e5321119
Ali Altaher <alialtaheralhamod@gmail.com>
2022-07-08 18:44:19 +0400

    week2 codes

---
+++ Crash_Course_on_Python/week2/area_Calc.py
@@ -0,0 +1,11 @@
+#  calculate the area of a triangle 
+#  of base 5, height 3 
+#  and output the result.  
+#  Reminder:  the area of a triangle is (base*height)/2
+
+base=5
+height=3
+
+area=(base*height)/2
+
+print(area)
